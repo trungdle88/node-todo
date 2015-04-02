52b22dbb37dd061485783df8d133dbbcb7ce7ca7
trungdle88 <trungdle88@gmail.com>
2015-04-02 13:29:39 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	 url:'mongodb://node:nodeuser@proximus.modulusmongo.net:27017/eqijA5ga'
+//	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
     //url:'mongodb://heroku_app35444036:Test123456@@ds059661.mongolab.com:59661/heroku_app35444036'
 }
